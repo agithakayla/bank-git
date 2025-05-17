50de36d4d383af50f868e8d1cf4b272d01dfa9b8
githaa <167840753+agithakayla@users.noreply.github.com>
2025-05-17 20:23:10 +0700

    Add files via upload

---
+++ db_atm.sql
@@ -0,0 +1,43 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 17, 2025 at 03:04 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_atm`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_bank`
+--
+-- Error reading structure for table db_atm.tb_bank: #1932 - Table 'db_atm.tb_bank' doesn't exist in engine
+-- Error reading data for table db_atm.tb_bank: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `db_atm`.`tb_bank`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_transaksi`
+--
+-- Error reading structure for table db_atm.tb_transaksi: #1932 - Table 'db_atm.tb_transaksi' doesn't exist in engine
+-- Error reading data for table db_atm.tb_transaksi: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `db_atm`.`tb_transaksi`' at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
